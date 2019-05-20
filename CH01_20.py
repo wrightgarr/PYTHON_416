bba6fb2ed5678365d49adcb7a6ff702788cbc0d8
wrightgarr <50820289+wrightgarr@users.noreply.github.com>
2019-05-20 18:43:55 +1200

    Rename CH01_20.py to Week20190508/CH01_20.py

--- CH01_20.py
+++
@@ -1,7 +0,0 @@
-def sharpe_ratio(Rb, Rf, sigma):
-
-	return (Rb - Rf)/sigma
-
-sr = sharpe_ratio(0.07, 0.03, 0.2)
-
-print("Given Rb = 0.07, Rf = 0.04, and a sigma of 0.2, the sharpe ration is: {0:.2f}".format(sr))
